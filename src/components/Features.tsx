ea804dc24f988d3355567264b7085ea2e9647813
Movin Tilakaratne <movin1999t@gmail.com>
2025-01-18 20:47:46 +0530

    Create BentoCard component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,3 +1,33 @@
+const BentoCard = ({
+  src,
+  title,
+  description,
+}: {
+  src: string;
+  title: React.ReactNode;
+  description: string;
+}) => {
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Features = () => {
   return (
     <section className="bg-black pb-52">
@@ -22,7 +52,6 @@ const Features = () => {
               </>
             }
             description="A cross-platform metagame app, turning your activites across Web2 and Web3 games into a rewarding adventure."
-            isComingSoon
           />
         </div>
       </div>
